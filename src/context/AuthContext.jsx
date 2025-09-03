49c594ff03888a34faa6d96820da18f22150832d
Miguel Rodrigues <miguel11rodrigues@gmail.com>
2025-09-03 13:56:16 +0100

    Axios config rota correta

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState, useEffect } from "react";
-import api from "../axiosConfig";
+import api from "../../axiosConfig";
 
 const AuthContext = createContext();
 
