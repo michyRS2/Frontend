e55a00af827634e02582a560c3c69924335ab97e
Miguel Rodrigues <miguel11rodrigues@gmail.com>
2025-09-04 16:52:20 +0100

    Correção percurso formativo

--- src/components/PercursoCard.jsx
+++ src/components/PercursoCard.jsx
@@ -9,16 +9,19 @@ const PercursoCard = ({ curso }) => {
     navigate(`/quiz/curso/${curso.ID_Curso}`);
   };
 
-  // Passamos uma prop para o CursoCard para ele renderizar "modo simples" sem ratings/botões
   return (
     <div className="PercursoCard-wrapper">
-      <CursoCard curso={{ ...curso, hideRatings: true, hideButtons: true }} />
-<button className="btn btn-primary btn-sm" onClick={handleVerPercurso}>
-  Ver Percurso Formativo
-</button>
-
+      <CursoCard 
+        curso={curso} 
+        hideRatings={true} 
+        hideButtons={true} 
+      />
+      <div className="text-center mt-2">
+        <button className="btn btn-primary btn-sm" onClick={handleVerPercurso}>
+          Ver Percurso Formativo
+        </button>
       </div>
-    
+    </div>
   );
 };
 
